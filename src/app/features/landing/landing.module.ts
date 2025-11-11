a8326567131998bc7b242df969aeafc4010d1a0b
WRIGHT Matthew L <matthew.wright@hexagon.com>
2025-11-11 00:42:46 -0500

    added public-navbar, footer, and signup-modal components for public landing page features. Declared within the landing module.

--- src/app/features/landing/landing.module.ts
+++ src/app/features/landing/landing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PublicNavbarComponent } from './components/public-navbar/public-navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SignupModalComponent } from './components/signup-modal/signup-modal.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PublicNavbarComponent,
+    FooterComponent,
+    SignupModalComponent
+  ],
   imports: [
     CommonModule
   ]

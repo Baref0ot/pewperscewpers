d267811ec727727687b19f8c1241cbd19d110472
WRIGHT Matthew L <matthew.wright@hexagon.com>
2025-11-10 21:46:17 -0500

    Added invoice-form componenet to billing module

--- src/app/features/billing/billing.module.ts
+++ src/app/features/billing/billing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { InvoiceFormComponent } from './components/invoice-form/invoice-form.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    InvoiceFormComponent
+  ],
   imports: [
     CommonModule
   ]
